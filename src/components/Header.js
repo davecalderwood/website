766d4548d83480b306d86a36d0bdd64b7b22c0f3
Dave Calderwood <davidhcalderwood@gmail.com>
2019-05-03 08:21:33 -0400

    Adding scrollable anchor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import ScrollableAnchor from 'react-scrollable-anchor'
+import { goToAnchor } from 'react-scrollable-anchor'
+ 
+export default class Page extends Component {
+  render() {
+        // scroll to #section1 without saving that hash update in history
+        goToAnchor('home')
+        goToAnchor('home', false)
+        
+        // scroll to #section1, saving that hash update in history
+        goToAnchor('home', true)
+    return (
+      <div>
+        <a href='#home'> Home </a>
+        <a href='#history'> History </a>
+        <a href='#about'> About </a>
+        <ScrollableAnchor id={'home'}>
+          <div></div>
+        </ScrollableAnchor>
+        <ScrollableAnchor id={'history'}>
+          <div></div>
+        </ScrollableAnchor>
+        <ScrollableAnchor id={'about'}>
+          <div></div>
+        </ScrollableAnchor>
 
 
-export default class Header extends Component {
-    render() {
-        return (
-            <div className="header">Header</div>
-        );
-    }
+      </div>
+    )
+  }
 }
